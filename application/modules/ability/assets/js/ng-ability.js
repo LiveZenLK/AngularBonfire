e6a3041eff843742fc5fa094da036685f96879a4
oi-sendai <binarygeometry@gmail.com>
2015-04-11 11:02:41 +0100

    like not yet, but if we have another module on a different page, it won't work

---
+++ application/modules/ability/assets/js/ng-ability.js
@@ -0,0 +1,14 @@
+
+var NgAbility = angular.module('NgAbility', []);
+
+NgAbility.controller('NgAbility', ['$scope', function($scope) {
+
+
+  	$scope.user ={
+		'name': 'Franz Kafka',
+		'coverletter': '<h3>I want to cut patterns</h3><p>Or something. Designing and manufacturing clothes is what I do best and it is what I love to do. I only have the experience I have given myself so I do not expect to be put in the design department from day one.</p>',
+		'cv': '<h3>A wisywig object</p>'
+	}
+
+}]);
+
