553b97d0ea292ad7dd6b0a4759acd056e414315c
oi-sendai <binarygeometry@gmail.com>
2015-04-16 15:37:43 +0100

    same query with active record

--- application/modules/profile/controllers/profile.php
+++ application/modules/profile/controllers/profile.php
@@ -26,11 +26,17 @@
                     where('user_id', $username)->
                     get('abilities')->result();
 
-            $sql = "SELECT id FROM bf_users WHERE username = ?";
+            $sql = "SELECT id userid FROM bf_users WHERE username = ? JOIN";
 
-$query = $this->db->query($sql, array($username))->result(); 
-$result = json_encode($query[0]);
-print_r($result);
+            $this->db->select('u.id userid');
+            $this->db->from('users u');
+            // $this->db->join('topics t', 't.user_id = u.user_id'); // this joins the user table to topics
+            // $this->db->join('quotes q', 'q.topic_id = t.topic_id'); // this joins the quote table to the topics table
+            $query = $this->db->get()->result();
+
+            // $query = $this->db->query($sql, array($username))->result(); 
+            $result = json_encode($query[0]);
+            print_r($result);
 
 
         }
