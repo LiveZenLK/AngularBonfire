7983823753f860228630bee7e12350d886eed93b
oi-sendai <binarygeometry@gmail.com>
2015-04-16 13:56:36 +0100

    some layout modifications

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,43 +2,26 @@
  
 
 <hr />
-
+	<h3>Example Work</h3>
 <div class="row-fluid">
-	<div class="span6">
-		<h4>Now with Angular JS</h4>
-
-		<p>This fork adds a sitewide capability to the core module system, allowing you to use as much Angular as you need.</p>
-
-		<p>Works seemlessly sideby side with existing php views and controllers. Also included is a simplified user registration process.</p>
+	<div class="span4">
+		<a href="http://bamford.co.uk/" alt="bamford"><img src="<?php echo site_url(); ?>/images/bamford.png"/></a>
+		<h4>Bamford</h4>
+		<p>Wordpress site built working as part of a small team at a design agency, I was responsible for much of the non ecommerce development work, front end and back, working towards the provided design.</p>
 	</div>
 
-	<div class="span6">
-		<h4>A Solid Base</h4>
+	<div class="span4">
+		<a href="http://www.steve-dinneen.com/" alt="steve dineen"><img src="<?php echo site_url(); ?>/images/steve-dineen.png"/></a>
+		<h4>Steve Dineen</h4>
+		<p>For this responsive Wordpress site I extended an off the shelf template to make it fully responsive and integrate the paperfold jquery plugin</p>
+	</div>
 
-		<p>Bonfire is based on <a href="http://ellislab.com/codeigniter" target="_blank">CodeIgniter <?php echo CI_VERSION; ?></a>, a proven PHP framework. In order to make the best use of it, you should be comfortable with CodeIgniter and its <a href="http://ellislab.com/codeigniter/user-guide/" target="_blank">documentation</a> first.</p>
 
-		<p>We use Twitter's <a href="">Bootstrap</a> front-end framework and <a href="http://jquery.com/">jQuery</a> as the basis of the CSS and Javascript.</p>
+	<div class="span4">
+		<a href="http://2tall.com" alt="2tall.com"><img src="<?php echo site_url(); ?>/images/2tall.png"/></a>
+		<h4>2tall Clothing</h4>
+		<p>Magento customised responsive theme with bespoke functionality built to high specifications working alongside the client to create the image he wanted for his brand</p>
 	</div>
 
 </div>
-
-<div class="row-fluid">
-
-	<div class="span6">
-		<h4>A Growing Community</h4>
-
-		<p>Bonfire has an ever-growing <a href="http://forums.cibonfire.com">community</a> of users that are there to help you get unstuck, or make the best use of this powerful system.</p>
-
-		<p>Bugs and feature discussion also happen on GitHub's <a href="https://github.com/ci-bonfire/Bonfire/issues?direction=desc&labels=0.7&sort=created&state=open">issue tracker</a>. This is the best place to report bugs and discuss new features.</p>
-	</div>
-	<div class="span6">
-		<h4>Built-in Flexibility</h4>
-
-		<p>A <a href="https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc">modular system</a> that allows code re-use, and overriding core modules with custom modules.</p>
-
-		<p>A <i>template system</i> that allows parent-child themes, and overriding controller views in the template.</p>
-
-		<p><i>Role-Based Access Control</i> that provides as much fine-grained control as your modules need.</p>
-	</div>
-
-</div>
+<hr />
--- public/themes/default/footer.php
+++ public/themes/default/footer.php
@@ -1,7 +1,53 @@
     <?php if ( ! isset($show) || $show == true) : ?>
     <hr />
-    <footer class="footer">
+    <footer class="footer footer-container">
         <div class="container">
+        <div class="row-fluid">
+    <div class="span3">
+        <h4>Now with Angular JS</h4>
+
+        <p>This fork adds a sitewide capability to the core module system, allowing you to use as much Angular as you need.</p>
+
+        <p>Works seemlessly sideby side with existing php views and controllers. Also included is a simplified user registration process.</p>
+    </div>
+
+    <div class="span3">
+        <h4>A Solid Base</h4>
+
+        <p>Bonfire is based on <a href="http://ellislab.com/codeigniter" target="_blank">CodeIgniter <?php echo CI_VERSION; ?></a>, a proven PHP framework. In order to make the best use of it, you should be comfortable with CodeIgniter and its <a href="http://ellislab.com/codeigniter/user-guide/" target="_blank">documentation</a> first.</p>
+
+        <p>We use Twitter's <a href="">Bootstrap</a> front-end framework and <a href="http://jquery.com/">jQuery</a> as the basis of the CSS and Javascript.</p>
+    </div>
+
+<!-- </div>
+
+<div class="row-fluid"> -->
+
+    <div class="span3">
+
+        <h4>A Growing Community</h4>
+
+        <hr/>
+        <h4>A Growing Community</h4>
+        <hr/>
+        <h4>A Growing Community</h4>
+        <hr/>
+        <h4>A Growing Community</h4>
+        <hr/>
+        <h4>A Growing Community</h4>
+    </div>
+    <div class="span3">
+        <hr/>
+        <h4>Built-in Flexibility</h4>
+
+        <p>A <a href="https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc">modular system</a> that allows code re-use, and overriding core modules with custom modules.</p>
+
+        <p>A <i>template system</i> that allows parent-child themes, and overriding controller views in the template.</p>
+
+        <p><i>Role-Based Access Control</i> that provides as much fine-grained control as your modules need.</p>
+    </div>
+
+</div>
             <p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire <?php echo BONFIRE_VERSION; ?></a></p>
         </div>
     </footer>
