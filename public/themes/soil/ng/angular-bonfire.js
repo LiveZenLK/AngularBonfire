af9f18499c8e3be6b87d19ca0080ce199ce8ed91
oi-sendai <binarygeometry@gmail.com>
2015-04-22 14:56:01 +0100

    part2.post1 'The Plan'

--- public/themes/soil/ng/angular-bonfire.js
+++ public/themes/soil/ng/angular-bonfire.js
@@ -1,1 +1,12 @@
-console.log('angular-bonfire.js')
+'use strict';
+console.log('angular-bonfire.js');
+
+// Declare app level module which depends on filters, and services
+var AngularBonfire = angular.module('AngularBonfire', 
+	[
+	'ui.router',
+	'ngAnimate',
+	// 'NgJoinCtrl'
+	// ,'ngResource','ngAnimate','ui.bootstrap','ngRoute','firebase'
+	]);
+
