d381c454d8a119686c06e320cbdf26f95890ce58
oi-sendai <binarygeometry@gmail.com>
2015-05-01 05:59:39 +0100

    changes

--- application/modules/chat/models/chat_model.php
+++ application/modules/chat/models/chat_model.php
@@ -23,4 +23,33 @@
             return true;
 
         }
+
+        public function get_messages($user_id=NULL)
+        {
+
+        	$messages = $this->db->
+                    where('username', $user_id)->
+                    get('chat')->result();
+
+            return $messages;
+        }
+
+        public function get_new_message($username)
+        {
+
+        	// do substitution username for userid in case user changes name
+        	$message = $this->db->
+                    where('username', $data['recipient_id'])->
+                    get('users')->result();
+
+            $foo = $abilities[0];
+
+            $data['recipient_id'] = $foo->id;
+
+
+            $this->db->insert('chat', $data); 
+            
+            return true;
+        }
+        
 }
