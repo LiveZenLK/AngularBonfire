fde96e73dd040dc4b6b8a75ef503c5b9a1598eb4
oi-sendai <binarygeometry@gmail.com>
2015-05-08 00:52:28 +0100

    started work on landing page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,15 +1,15 @@
 <main class="container minimal-modern">
 <div class="row">
-	<div class="col-4 col-desktop-4 left-modern">
+	<div class="col-4 col-desktop-12 left-modern">
 		<article class="modern">
-		<h1 class="goodbye-world">unstructured networking</h1>
+		<h1 class="goodbye-world">unstructured<br/>-networking</h1>
 		</article>
 		<?php echo Modules::run('join/cta', 1); ?>
 	</div>
-	<div class="col-12 col-desktop-8 right-modern" style="background-image:url('<?php echo site_url(); ?>/images/gardenpace.png')">
-		<?php echo Modules::run('frontpage/template'); ?>
+	<!-- <div class="col-12 col-desktop-8 right-modern" style="background-image:url('<?php //echo site_url(); ?>/images/gardenpace.png')">
+		<?php //echo Modules::run('frontpage/template'); ?>
 	</div>
-
+ -->
 </div>
 </main>
 <div class="row">
